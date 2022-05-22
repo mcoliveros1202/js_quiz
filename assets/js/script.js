964b141c5192aed23a991df44ec3c937a8f498ab
Moogs <mcoliveros1202@gmail.com>
2022-05-21 20:30:31 -0500

    start javascript, defined variables, established start button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,24 @@
-var startButton = document.getElementById("startButton")
-function startQuiz () {
-    var startScreenEl = document.getElementById("quiz-start");
+var startButton = document.querySelector(".start-btn button");
+var quizBox = document.querySelector(".quiz-box");
+// var startButton = document.querySelector(".start-btn button");
+// var startButton = document.querySelector(".start-btn button");
+// var startButton = document.querySelector(".start-btn button");
+// var startButton = document.querySelector(".start-btn button");
+var submitButton = document.querySelector(".buttons .submit");
 
-    startScreenEl.setAttribute("class", "hide");
-    console.log("test")
+// if start quiz button clicked
+startButton.onclick = ()=>{
+    quizBox.classList.add("activeInfo");
+    // console.log("test");
 }
 
-startButton.onclick = startQuiz;
+// function startQuiz () {
+//     var startScreenEl = document.getElementById("quiz-start");
+
+//     startScreenEl.setAttribute("class", "hide");
+//     console.log("test")
+// }
+
+// startButton.onclick = startQuiz;
+
+// var 
