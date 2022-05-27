a9916fbd05b88142f0bc83c4d41b9243239ac59d
moliveros <mcoliveros1202@gmail.com>
2022-05-27 12:16:23 -0500

    reformat HTML, CSS, and JS

---
+++ assets/js/game.js
@@ -0,0 +1,108 @@
+const question = document.getElementById("question");
+const choices = Array.from(document.getElementsByClassName("choice-text"));
+
+let currentQuestion = {};
+let acceptingAnswers = true;
+let score = 60;
+let questionCounter = 0;
+let availableQuestions = [];
+
+let questions = [
+    {
+        question: "Inside which HTML element do we put the JavaScript?",
+        choice1: "javascript",
+        choice2: "js",
+        choice3: "script",
+        choice4: "scripting",
+        answer: 3
+
+    },
+    {
+        question: "How do you write 'Hello World' in an alert box?",
+        choice1: "alert('Hello World')",
+        choice2: "msgBox('Hello World')",
+        choice3: "alertBox='Hello World'",
+        choice4: "alertBox('Hello World')",
+        answer: 1
+
+    },
+    {
+        question: "How do you write a conditional statement for executing some statements only if 'i' is equal to 5?",
+        choice1: "if i == 5 then",
+        choice2: "if(i== 5)",
+        choice3: "if i = 5 then",
+        choice4: "if i = 5",
+        answer: 2
+    },
+    {
+        question: "How does a 'for' loop start?",
+        choice1: "for(i = 0; i <= 5)",
+        choice2: "for (i = 0; i <= 5; i++)",
+        choice3: "for i = 1 to 5",
+        choice4: "for (i <= 5; i++)",
+        answer: 2
+
+    },
+    {
+        question: "How can you add a comment in a JavaScript?",
+        choice1: "//This is a comment",
+        choice2: "'This is a comment",
+        choice3: "< !--This is a comment-- >",
+        choice4: "#This is a comment",
+        answer: 1
+    }
+];
+
+// CONSTANTS
+const INCORRECT_BONUS = -15;
+const MAX_QUESTIONS = 5
+
+startGame = () => {
+    questionCounter = 0;
+    score = 60;
+    availableQuestions = [...questions];
+    getNewQuestion();
+};
+
+getNewQuestion = () => {
+    if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+        //got to the end page
+        return window.location.assign();
+    }
+    questionCounter++;
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionIndex];
+    question.innerText = currentQuestion.question;
+
+    choices.forEach(choice => {
+        const number = choice.dataset['number'];
+        choice.innerText = currentQuestion['choice' + number];
+    });
+
+    availableQuestions.splice(questionIndex, 1);
+
+    acceptingAnswers = true;
+};
+
+choices.forEach(choice => {
+    choice.addEventListener("click", e => {
+        if (!acceptingAnswers) return;
+
+        acceptingAnswers = false;
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.dataset["number"];
+
+        const classToApply =
+            selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+
+
+        selectedChoice.parentElement.classList.add(classToApply);
+
+        setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(classToApply);
+            getNewQuestion();
+        }, 1000);
+    });
+});
+
+startGame();
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,185 +1,176 @@
-const startButton = document.getElementById('start-btn');
-const introBox = document.getElementById('intro-box');
-const questionContainer = document.getElementById('quiz-box');
-const questionElement = document.getElementById('question')
-const answerButtonsElement = document.getElementById('answer-btn')
-const resultsBox = document.getElementById('results')
+// const startButton = document.getElementById('start-btn');
+// const introBox = document.getElementById('intro-box');
+// const questionContainer = document.getElementById('quiz-box');
+// const questionElement = document.getElementById('question')
+// const answerButtonsElement = document.getElementById('answer-btn')
+// const resultsBox = document.getElementById('results')
+// const submit = document.getElementById('submit')
+// const backButton = document.getElementById('back-btn')
+// const clearButton = document.getElementById('clear-btn')
 
-let shuffledQuestions, currentQuestionIndex
+// let shuffledQuestions, currentQuestionIndex
 
 
-var timerEl = document.getElementById('timer-sec');
+// var timerEl = document.getElementById('timer-sec');
 
-// function countdown() {
-//     var timeLeft = 60;
-//     timeLeft = TimeoutID(function () {
-//         timeLeft--;
-//         if (timeLeft > 1) {
-//             timerEl.textContent = timeLeft;
-//             timeLeft--;
+// function startTimer(duration, display) {
+//     var timer = duration, seconds;
+//     setInterval(function () {
+
+//         seconds = parseInt(timer % 60, 10);
+
+
+//         seconds = seconds < 10 ? "0" + seconds : seconds;
+
+//         display.textContent = seconds;
+
+//         if (--timer < 0) {
+//             timer = duration;
 //         }
-//         else if (timeLeft === 1) {
-//             timerEl.textContent = timeLeft;
-//             timeLeft--;
-//         }
-//         else {
-//             timerEl.textContent = "";
-//             resultsBox.classList.remove('hide');
-//             questionContainer.classList.add('hide');
-
-//         }
-//         console.log('timer is working');
-//     })
+//     }, 1000);
+    
+//     function myStopFunction() {
+//         clearTimer(timer);
+//     }
+//     document.querySelectorAll('correct: false').addEventListener('click', function () {
+//         sec -= 15;
+//         document.getElementById('timer').innerHTML = '00:' + sec;
+//     });
 // }
 
-function startTimer(duration, display) {
-    var timer = duration, seconds;
-    setInterval(function () {
-
-        seconds = parseInt(timer % 60, 10);
-
-        
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-
-        display.textContent = seconds;
-
-        if (--timer < 0) {
-            timer = duration;
-        }
-    }, 1000);
-}
 
 
+// startButton.addEventListener('click', startGame)
+// answerButtonsElement.addEventListener('click', () => {
+//     currentQuestionIndex++
+//     setNextQuestion();
 
-startButton.addEventListener('click', startGame)
-answerButtonsElement.addEventListener('click', () => {
-    currentQuestionIndex++
-    setNextQuestion();
-    
-})
-function timerTrigger() {
-    var time = 60,
-        display = document.querySelector('#timer');
-    startTimer(time, display);
-};
-function startGame() {
-    startButton.classList.add('hide');
-    introBox.classList.add('hide');
-    shuffledQuestions = questions.sort(() => Math.random() - .5);
-    currentQuestionIndex = 0;
-    questionContainer.classList.remove('hide');
-    setNextQuestion();
+// })
+// function timerTrigger() {
+//     var time = 60,
+//         display = document.querySelector('#timer');
+//     startTimer(time, display);
+// };
+// function startGame() {
+//     startButton.classList.add('hide');
+//     introBox.classList.add('hide');
+//     shuffledQuestions = questions.sort(() => Math.random() - .5);
+//     currentQuestionIndex = 0;
+//     questionContainer.classList.remove('hide');
+//     setNextQuestion();
 
-    timerTrigger();
-    startTimer();
-}
+//     timerTrigger();
+//     startTimer();
+// }
 
-function setNextQuestion() {
-    resetState()
-    showQuestion(shuffledQuestions[currentQuestionIndex])
-}
+// function setNextQuestion() {
+//     resetState()
+//     showQuestion(shuffledQuestions[currentQuestionIndex])
+// }
 
-function showQuestion(question) {
-    questionElement.innerText = question.question
-    question.choices.forEach(answer => {
-        const button = document.createElement('button')
-        button.innerText = answer.text
-        button.classList.add('answer-btn')
-        if (answer.correct) {
-            button.dataset.correct = answer.correct
-        }
-        button.addEventListener('click', selectAnswer)
-        answerButtonsElement.appendChild(button)
-    });
-}
+// function showQuestion(question) {
+//     questionElement.innerText = question.question
+//     question.choices.forEach(answer => {
+//         const button = document.createElement('button')
+//         button.innerText = answer.text
+//         button.classList.add('answer-btn')
+//         if (answer.correct) {
+//             button.dataset.correct = answer.correct
+//         }
+//         button.addEventListener('click', selectAnswer)
+//         answerButtonsElement.appendChild(button)
+//     });
+// }
 
-function resetState() {
-    while (answerButtonsElement.firstChild) {
-        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
-    }
-}
+// function resetState() {
+//     while (answerButtonsElement.firstChild) {
+//         answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+//     }
+// }
 
-function selectAnswer(e) {
-    const selectedButton = e.target
-    const correct = selectedButton.dataset.correct
-    setStatusClass(document.body, correct)
-    Array.from(answerButtonsElement.children).forEach(button => {
-        setStatusClass(button, button.dataset.correct)
-    })
-    if (shuffledQuestions.length > currentQuestionIndex + 1) {
-        // SHOW NEXT QUESTIONS
-    } else {
-        resultsBox.classList.remove('hide')
-        questionContainer.classList.add('hide')
-    }
-}
+// function selectAnswer(e) {
+//     const selectedButton = e.target
+//     const correct = selectedButton.dataset.correct
+//     setStatusClass(document.body, correct)
+//     Array.from(answerButtonsElement.children).forEach(button => {
+//         setStatusClass(button, button.dataset.correct)
+//     })
+//     if (shuffledQuestions.length > currentQuestionIndex + 1) {
+//         // SHOW NEXT QUESTIONS
+//     } else {
+//         resultsBox.classList.remove('hide')
+//         questionContainer.classList.add('hide') 
+//     }
+// }
 
-function setStatusClass(element, correct) {
-    clearStatusClass(element)
-    if (correct) {
-        element.classList.add('correct')
-    } else {
-        element.classList.add('wrong')
-    }
-}
+// // could be deleted?
+// function setStatusClass(element, correct) {
+//     clearStatusClass(element)
+//     if (correct) {
+//         element.classList.add('correct')
+//     } else {
+//         element.classList.add('wrong')
+//         // subtract 15 seconds?
+//     }
+// }
+// // reset colors
+// function clearStatusClass(element) {
+//     element.classList.remove('correct')
+//     element.classList.remove('wrong')
+// }
 
-function clearStatusClass(element) {
-    element.classList.remove('correct')
-    element.classList.remove('wrong')
-}
+// var questions = [
+//     {
+//         question: "Inside which HTML element do we put the JavaScript?",
+//         choices: [
+//             { text1: 'javascript', correct: false },
+//             { text2: 'js', correct: false },
+//             { text3: 'script', correct: true },
+//             { text4: 'scripting', correct: false }
+//         ]
+//     },
 
-var questions = [
-    {
-        question: "Inside which HTML element do we put the JavaScript?",
-        choices: [
-            { text: 'javascript', correct: false },
-            { text: 'js', correct: false },
-            { text: 'script', correct: true },
-            { text: 'scripting', correct: false }
-        ]
-    },
+//     {
+//         question: "How do you write 'Hello World' in an alert box?",
+//         choices: [
+//             { text1: "alert('Hello World')", correct: true },
+//             { text2: "msgBox('Hello World')", correct: false },
+//             { text3: "alertBox='Hello World'", correct: false },
+//             { text4: "alertBox('Hello World')", correct: false }
+//         ]
 
-    {
-        question: "How do you write 'Hello World' in an alert box?",
-        choices: [
-            { text: "alert('Hello World')", correct: true },
-            { text: "msgBox('Hello World')", correct: false },
-            { text: "alertBox='Hello World'", correct: false },
-            { text: "alertBox('Hello World')", correct: false }
-        ]
+//     },
+//     {
+//         question: "How do you write a conditional statement for executing some statements only if 'i' is equal to 5?",
+//         choices: [
+//             { text1: "if i == 5 then", correct: false },
+//             { text2: "if(i== 5)", correct: true },
+//             { text3: "if i = 5 then", correct: false },
+//             { text4: "if i = 5", correct: false }
+//         ],
 
-    },
-    {
-        question: "How do you write a conditional statement for executing some statements only if 'i' is equal to 5?",
-        choices: [
-            { text: "if i == 5 then", correct: false },
-            { text: "if(i== 5)", correct: true },
-            { text: "if i = 5 then", correct: false },
-            { text: "if i = 5", correct: false }
-        ],
+//     },
 
-    },
+//     {
+//         questions: "How does a 'for' loop start?",
+//         choices: [
+//             { text1: "for(i = 0; i <= 5)", correct: false },
+//             { text2: "for (i = 0; i <= 5; i++)", correct: true },
+//             { text3: "for i = 1 to 5", correct: false },
+//             { text4: "for (i <= 5; i++)", correct: false }
+//         ]
+//     },
 
-    {
-        questions: "How does a 'for' loop start?",
-        choices: [
-            { text: "for(i = 0; i <= 5)", correct: false },
-            { text: "for (i = 0; i <= 5; i++)", correct: true },
-            { text: "for i = 1 to 5", correct: false },
-            { text: "for (i <= 5; i++)", correct: false }
-        ]
-    },
-
-    {
-        question: "How can you add a comment in a JavaScript?",
-        choices: [
-            { text: "//This is a comment", correct: true },
-            { text: "'This is a comment", correct: false },
-            { text: "< !--This is a comment-- >", correct: false },
-            { text: "#This is a comment", correct: false }
-        ],
-    }
-];
+//     {
+//         question: "How can you add a comment in a JavaScript?",
+//         choices: [
+//             { text1: "//This is a comment", correct: true },
+//             { text2: "'This is a comment", correct: false },
+//             { text3: "< !--This is a comment-- >", correct: false },
+//             { text4: "#This is a comment", correct: false }
+//         ],
+//     }
+// ];
 
 
 // TO DO:
