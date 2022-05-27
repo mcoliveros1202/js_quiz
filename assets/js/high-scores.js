d6472f3cae53c13d852c67b1ea37a8c806154cf5
moliveros <mcoliveros1202@gmail.com>
2022-05-27 17:39:01 -0500

    reverting

--- assets/js/high-scores.js
+++ assets/js/high-scores.js
@@ -1,17 +1,12 @@
 const highScoresList = document.getElementById("highScoresList");
 const highScores = JSON.parse(localStorage.getItem("highScores") || []);
-const clearList = document.getElementById("clear-btn");
-
+const clearList = document.getElementsByClassName("btn");
 highScoresList.innerHTML = highScores
 .map(score => {
     return `<li class="high-score">${score.name} - ${score.score}</li>`;
 })
 .join("");
 
-clearList.addEventListener("click", clearScoresList);
-
-function clearScoresList(){
-    localStorage.clear();
+function removeAll(){
     document.getElementById("highScoresList").innerHTML = "";
-};
-
+}
