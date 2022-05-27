583b6dcd36e8864e08a3d8e06831594ad601c8ba
moliveros <mcoliveros1202@gmail.com>
2022-05-27 15:24:30 -0500

    fixed clear button

--- assets/js/high-scores.js
+++ assets/js/high-scores.js
@@ -1,12 +1,17 @@
 const highScoresList = document.getElementById("highScoresList");
 const highScores = JSON.parse(localStorage.getItem("highScores") || []);
-const clearList = document.getElementsByClassName("btn");
+const clearList = document.getElementById("clear-btn");
+
 highScoresList.innerHTML = highScores
 .map(score => {
     return `<li class="high-score">${score.name} - ${score.score}</li>`;
 })
 .join("");
 
-function removeAll(){
+clearList.addEventListener("click", clearScoresList);
+
+function clearScoresList(){
+    localStorage.clear();
     document.getElementById("highScoresList").innerHTML = "";
-}
+};
+
